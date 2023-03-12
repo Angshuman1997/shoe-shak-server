c63f6dcd400af963d207cfc56f13bc88bfbab4dc
Angshuman Bardhan <bardhanangshu@gmail.com>
2023-03-12 22:04:50 +0530

    test endpoint added

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const Product = require("./database");
 const app = express();
 
 app.use(express.json());
+
+app.get("/server-test", async (req, resp) => {
+    resp.send({"Message": "Server Up and Running"});
+  });
+
 app.post("/create", async (req, resp) => {
   let data = new Product(req.body);
   const result = await data.save();
@@ -18,13 +23,11 @@ app.get("/list", async (req, resp) => {
 });
 
 app.delete("/delete/:_id", async (req, resp) => {
-  console.log(req.params);
   let data = await Product.deleteOne(req.params);
   resp.send(data);
 });
 
 app.put("/update/:_id", async (req, resp) => {
-  console.log(req.params);
   let data = await Product.updateOne(req.params, { $set: req.body });
   resp.send(data);
 });
